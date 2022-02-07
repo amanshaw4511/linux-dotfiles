e490f57eb4859eb5db332686766d6dc75424798d
amanshaw4511 <amanshaw4511@protonmail.com>
2022-02-07 21:38:18 +0530

    updated nvim for lua plugins

---
+++ nvim/lua/rc_lspconfig.lua
@@ -0,0 +1,83 @@
+local servers = {'rust_analyzer' }
+
+local on_attach = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", {buffer=0})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {buffer=0})
+        vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", {buffer=0}) vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", {buffer=0}) vim.keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<cr>", {buffer=0})
+
+        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.formatting, {buffer=0})
+        vim.keymap.set("n", "<leader>crn", vim.lsp.buf.rename, {buffer=0})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
+        vim.keymap.set("n", "<leader>cd", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+        vim.keymap.set("n", "<leader>cdj", vim.diagnostic.goto_next, {buffer=0})
+        vim.keymap.set("n", "<leader>cdk", vim.diagnostic.goto_prev, {buffer=0})
+
+        -- auto completion
+        vim.keymap.set("i", "c-<space>", vim.lsp.omnifunc, {buffer=0})
+    end
+
+  -- Setup nvim-cmp.
+vim.opt.completeopt = {"menu", "menuone", "noselect"}
+
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    mapping = {
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+      ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    },
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' }, -- For luasnip users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline('/', {
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+for _,lsp in ipairs(servers) do
+    require('lspconfig')[lsp].setup{
+        capabilities = capabilities,
+        on_attach = on_attach
+    }
+end
+
+-- nnoremap <leader>cD :Telescope coc definitions
+-- nnoremap <leader>cT :Telescope coc type_definitions
+-- nnoremap <leader>cI :Telescope coc implementations
+-- nnoremap <leader>cR :Telescope coc references
+
+-- nnoremap <leader>cd :Telescope coc diagnostics<CR>
+-- nnoremap <leader>cs :Telescope coc document_symbols
+

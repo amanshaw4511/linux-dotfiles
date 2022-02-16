2a7598798c6e7159eb3431e687265b9dc8997095
amanshaw4511 <amanshaw4511@protonmail.com>
2022-02-16 20:39:26 +0530

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 
-cp -r ~/.config/nvim nvim
-cp ~/.zshrc ~/.myzshrc zsh
-cp ~/.tmux.conf tmux
+cp -r ~/.config/nvim nvim && echo "nvim done ..."
+
+cp ~/.zshrc ~/.myzshrc zsh && echo "zsh done ..."
+
+cp ~/.tmux.conf tmux && echo "tmux done..."
 
 git add .
 git commit -m "update"
